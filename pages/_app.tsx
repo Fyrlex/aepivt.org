ab48b34c259119c12e03332168336961f99b603a
Jaren Goldberg <fyrlex@gmail.com>
2025-03-03 12:18:02 -0500

    fix: packages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,15 @@ import '../public/styles/globals.css';
 import { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
 
+import { Analytics } from '@vercel/analytics/next';
+
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps): JSX.Element {
   return (
     <>
       <SessionProvider session={session}>
         <div className='fade-in'>
           <Component {...pageProps} />
+          <Analytics />
         </div>
       </SessionProvider>
     </>
